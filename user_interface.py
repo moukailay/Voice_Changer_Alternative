94162021a40a0f62fe1759cf885220db4b8dc176
Moukaïla <60848744+moukailay@users.noreply.github.com>
2023-11-25 01:11:33 -0500

    implémenttion des methodes

--- user_interface.py
+++ user_interface.py
@@ -1,3 +1,5 @@
+import wave
+import requests
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QProgressBar
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QIcon
@@ -49,25 +51,79 @@ class VoiceRecorderUI(QWidget):
         self.downloadButton.clicked.connect(self.onDownload)
 
     def onRecord(self):
-        # Écrire la Logique pour commencer l'enregistrement
-        pass
+        self.isRecording = True
+        self.messageLabel.setText("Enregistrement en cours...")
+
+        self.stream = self.pyaudio.open(format=self.format, channels=self.channels,
+                                        rate=self.rate, input=True,
+                                        frames_per_buffer=self.chunk_size)
+        self.frames = []
+
+        while self.isRecording:
+            data = self.stream.read(self.chunk_size)
+            self.frames.append(data)
 
     def onStop(self):
-        # Logique pour arrêter l'enregistrement
-        pass
+        self.isRecording = False
+        self.messageLabel.setText("Enregistrement arrêté")
+
+        self.stream.stop_stream()
+        self.stream.close()
+
+        self.pyaudio.terminate()
+
+        # Sauvegarde de l'enregistrement dans un fichier
+        with wave.open('enregistrement.wav', 'wb') as wf:
+            wf.setnchannels(self.channels)
+            wf.setsampwidth(self.pyaudio.get_sample_size(self.format))
+            wf.setframerate(self.rate)
+            wf.writeframes(b''.join(self.frames))
 
     def onPlay(self):
-        # Logique pour jouer l'enregistrement
-        pass
+        self.messageLabel.setText("Lecture de l'enregistrement")
+
+        # Jouer le fichier 'enregistrement.wav'
+        with wave.open('enregistrement.wav', 'rb') as wf:
+            stream = self.pyaudio.open(format=self.pyaudio.get_format_from_width(wf.getsampwidth()),
+                                       channels=wf.getnchannels(),
+                                       rate=wf.getframerate(),
+                                       output=True)
+
+            data = wf.readframes(self.chunk_size)
+            while data:
+                stream.write(data)
+                data = wf.readframes(self.chunk_size)
+
+            stream.stop_stream()
+            stream.close()
 
     def onUpload(self):
-        # Logique pour uploader l'enregistrement
-        pass
+        self.messageLabel.setText("Upload en cours...")
+
+        # Remplacer par l'URL de votre API ou service d'upload
+        url = "http://monapi.com/upload"
+        files = {'file': open('enregistrement.wav', 'rb')}
+
+        response = requests.post(url, files=files)
+
+        if response.status_code == 200:
+            self.messageLabel.setText("Upload réussi")
+        else:
+            self.messageLabel.setText("Erreur d'upload")
 
     def onDownload(self):
-        # Logique pour télécharger l'enregistrement
-        pass
+        self.messageLabel.setText("Téléchargement en cours...")
 
+        # TODO: Remplacer par l'URL du fichier à télécharger
+        url = "http://monapi.com/enregistrement.wav"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            with open('enregistrement_telecharge.wav', 'wb') as f:
+                f.write(response.content)
+            self.messageLabel.setText("Téléchargement réussi")
+        else:
+            self.messageLabel.setText("Erreur de téléchargement")
 if __name__ == '__main__':
     app = QApplication([])
     ex = VoiceRecorderUI()
